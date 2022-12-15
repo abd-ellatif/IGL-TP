3a29596e0d508df1f7a498a5ab855af80936ef86
abd-ellatif <ka_bendjeddou@esi.dz>
2022-12-15 19:19:57 +0100

    sd

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import Button from "./Components/Button";
-import HeroPage from "./Components/HeroPage";
-import HeroSection from "./Components/HeroSection";
-import LogInNavBar from "./Components/logInNavBar";
-import NavBar from "./Components/NavBar";
+import SignUpSection from "./Components/SignUpSection";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <HeroPage></HeroPage>
+      <SignUpSection></SignUpSection>
     </div>
   );
 }
---
+++ src/Components/SignUpSection.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
+
+class SignUpSection extends Component {
+    state = {  } 
+    render() { 
+        return (
+        <div className="bg-gray-200 w-full h-fit flex flex-col place-items-center justify-evenly ">
+        <h className="mt-40">NOUVEAU COMPTE</h>
+        <div className="w-10 h-10 bg-gray-500">
+           
+        </div>
+                
+        </div>); 
+    }
+}
+ 
+export default SignUpSection;
